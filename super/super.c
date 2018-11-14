9b59bfd456417035222e71a7bee3ea38f58265da
lmunier <louis.munier@epfl.ch>
2018-11-14 11:02:39 +0100

    Initialize DIS repository

---
+++ super/super.c
@@ -0,0 +1,10 @@
+// Supervisor of the project
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <webots/robot.h>
+#include <webots/emitter.h>
+#include <webots/supervisor.h>
