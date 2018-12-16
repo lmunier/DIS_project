48a8a8322588abc53e08bed56e852eb6d7396fc2
Munier Louis <louis.munier@epfl.ch>
2018-12-16 13:25:24 +0100

    Test to recover metric saving.

--- controllers/super/super.c
+++ controllers/super/super.c
@@ -10,7 +10,7 @@
 #include <webots/supervisor.h>
 
 #define METRICS
-#define FLOCK_SIZE	10		// Number of robots in flock
+#define FLOCK_SIZE	5		// Number of robots in flock
 #define TIME_STEP	64		// [ms] Length of time step
 #define fit_cluster_ref 0.03
 #define fit_orient_ref 1.0
